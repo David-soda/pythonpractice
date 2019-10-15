b494e359c3aa6621500754f2453f0ef97e5065a3
wudawei <1462736023@qq.com>
2019-10-16 00:08:21 +0800

    increase high function

--- practice.py
+++ practice.py
@@ -1,5 +1,13 @@
 def arerage(m,n):
     return (m+n)/2
+def sum(a,b,*c):
+    total=a+b
+    for n in c:
+        total=total+n
+    return total
 a=1;b=2
 a,b=b,a
 print(a+arerage(a,b))
+print(sum(1,2))
+print(sum(1,2,3,4))
+print(sum(1,2,3,4,5,6))
