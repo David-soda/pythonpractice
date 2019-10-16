396c7b43216483149b64b7c977e6082a5edf5eed
wudawei <1462736023@qq.com>
2019-10-16 13:00:01 +0800

    increase max

--- practice.py
+++ practice.py
@@ -5,9 +5,16 @@ def sum(a,b,*c):
     for n in c:
         total=total+n
     return total
+def max(a,b,*c):
+    max_value=a
+    if max_value<b:
+        max_value=b
+    for n in c:
+        if max_value<n:
+            max_value=n
+    return max_value
 a=1;b=2
 a,b=b,a
 print(a+arerage(a,b))
 print(sum(1,2))
-print(sum(1,2,3,4))
-print(sum(1,2,3,4,5,6))
+print(max(1,2,3,4,5,6))
